5ee40760bcf8411bc01bba6fe973049e9f04687f
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-08-05 22:54:38 -0700

    first commit

--- src/Admin/Uploads/Button.js
+++ src/Admin/Uploads/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='mt-[25px] py-1 mb-2 focus:outline-0 px-5 hover:border-transparent hover:text-white font-jost text-center duration-300 hover:border-[0.2px] rounded-lg bg-indigo-800'>
+    <button className='mt-[25px] py-1 mb-2 focus:outline-0 px-5 text-white hover:border-transparent hover:text-white font-jost text-center duration-300 hover:border-[0.2px] rounded-lg bg-indigo-800'>
         {props.name}
     </button>
   )
--- src/Client/ViewLecture/Button.js
+++ src/Client/ViewLecture/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='mt-[25px] focus:outline-0 py-1 px-5 hover:border-transparent hover:text-white text-center duration-300 hover:border-[1px] rounded-lg bg-indigo-800'>
+    <button className='mt-[25px] focus:outline-0 py-1 px-5 hover:border-transparent text-white hover:text-white text-center duration-300 hover:border-[1px] rounded-lg bg-indigo-800'>
         {props.name}
     </button>
   )
