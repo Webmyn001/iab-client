aba30cb40aee3c8cdcc7c791e4f1a3f468ec7833
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-09-01 03:24:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router ,Route, Routes, } from "react-router-dom"
 
-import Navbar from './IAB/Admin/header/Navbar'
-import Footer from './IAB/Admin/Footer/Footer'
-import Homepage from './IAB/Admin/Homepage'
+import Navbar from './IAB/Client/header/Navbar'
+import axios from 'axios'
+import Footer from './IAB/Client/Footer/Footer'
+import Homepage from './IAB/Client/Homepage'
+import Contact from './IAB/Client/Contact'
 
 
 function App() {
+  const [Lecture, setLecture] =useState([])
+  const [loading, setLoading] = useState(false)
+
+  const getLecture = async  () => {
+     axios.get("https://naughty-wasp-umbrella.cyclic.app/api/mssnoau/iab/")
+     .then(res => {
+       console.log(res.data)
+       setLecture(res.data)
+       setLoading(true)
+     }).catch(err => {
+       console.log(err)
+     })
+    }
+
+useEffect(() => {
+     getLecture()
+     }, [])
+
 
      
   return (
@@ -17,12 +37,9 @@ function App() {
        
          <Routes>
 
-           {/* Admin */}
-           
-               <Route path = "/" exact element= {<Homepage />}></Route>
-                   
-                   {/* List to delete */}
-
+            {/* Client */}
+            <Route path = "/" exact element= {<Homepage Lecture={Lecture} loading={loading}/>}></Route>
+               <Route path = "/contact" exact element= {<Contact/>}></Route>
          </Routes>
        <Footer/>
          </Router> 
