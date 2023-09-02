2eb1b7fb244ebe65ecc82199c7eadf31d2737eb2
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-09-02 18:34:29 +0100

    first commit

--- src/IAB/Admin/header/Navlinks.js
+++ src/IAB/Admin/header/Navlinks.js
@@ -8,7 +8,7 @@ import {Link} from "react-router-dom";
         {name: "Home",
          link : "/"
         },
-       {name: "",
+       {name: "Reminder Lists",
        link : "/list"
       },
        
